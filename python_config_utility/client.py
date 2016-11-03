6acd6a26a3e1ecb2d00f7153dcdd257aaa884b14
alxshine <alexander.schloegl@student.uibk.ac.at>
2016-11-03 18:53:31 +0100

    changed server to switch port, made client.py a test utility

--- python_config_utility/client.py
+++ python_config_utility/client.py
@@ -1,10 +1,10 @@
 import socket
+import sys
 
 UDP_TARGET = "localhost"
-UDP_PORT = 1337
 MESSAGE = "AOEU"
 
-print("sending: " + MESSAGE + " to " + UDP_TARGET)
+port = int(sys.argv[1])
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(MESSAGE, (UDP_TARGET,UDP_PORT))
+sock.sendto(MESSAGE, (UDP_TARGET,port))
--- python_config_utility/server.py
+++ python_config_utility/server.py
@@ -1,10 +1,21 @@
 import socket
 
-UDP_PORT = 1337
+FALSE_PORT = 1337
+TRUE_PORT = 1338
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('', UDP_PORT))
+stp = False
+sockFalse = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sockFalse.bind(('', FALSE_PORT))
+
+sockTrue = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sockTrue.bind(('', TRUE_PORT))
 
 while True:
-    data, addr = sock.recvfrom(1024)
-    print("received message: ", data)
+    if stp:
+        data, addr = sockTrue.recvfrom(1024)
+        print("deactivating STP")
+    else:
+        data, addr = sockFalse.recvfrom(1024)
+        print("activating STP")
+
+    stp = not stp
