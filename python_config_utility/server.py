64d79fe199c12492fe66ac676d53ca76cbcf1aa4
alxshine <alexander.schloegl@student.uibk.ac.at>
2017-03-02 15:00:46 +0100

    Made the server start up with stp enabled to make presentation easier

--- python_config_utility/server.py
+++ python_config_utility/server.py
@@ -6,14 +6,14 @@ from subprocess import call
 FALSE_PORT = 1337
 TRUE_PORT = 1338
 
-stp = False
+stp = True
 sockFalse = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sockFalse.bind(('', FALSE_PORT))
 
 sockTrue = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sockTrue.bind(('', TRUE_PORT))
 
-call(["cp", "/etc/config/noStp", "/etc/config/network"])
+call(["cp", "/etc/config/stp", "/etc/config/network"])
 call(["/etc/init.d/network", "restart"])
 
 while True:
