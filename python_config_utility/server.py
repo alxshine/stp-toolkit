082ac2375e76255137404820b233975b85558df5
alxshine <alexander.schloegl@student.uibk.ac.at>
2017-03-03 16:04:52 +0100

    Changed the server.py to use the python wrapper for C system, and recreated the sockets after the network service restart

--- python_config_utility/server.py
+++ python_config_utility/server.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python
 
-import socket
-from subprocess import call
+from os import system
+from socket import *
 
 FALSE_PORT = 1337
 TRUE_PORT = 1338
 
 stp = True
-sockFalse = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sockFalse.bind(('', FALSE_PORT))
 
-sockTrue = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sockTrue.bind(('', TRUE_PORT))
 
-call(["cp", "/etc/config/stp", "/etc/config/network"])
-call(["/etc/init.d/network", "restart"])
+system("cp /etc/config/stp /etc/config/network")
+system("/etc/init.d/network restart")
 
 while True:
     if stp:
+        sockTrue = socket(AF_INET, SOCK_DGRAM)
+        sockTrue.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        sockTrue.bind(('', TRUE_PORT))
         data, addr = sockTrue.recvfrom(1024)
-        call(["cp", "/etc/config/noStp", "/etc/config/network"])
+        system("cp /etc/config/noStp /etc/config/network")
     else:
+        sockFalse = socket(AF_INET, SOCK_DGRAM)
+        sockFalse.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        sockFalse.bind(('', FALSE_PORT))
         data, addr = sockFalse.recvfrom(1024)
-        call(["cp", "/etc/config/stp", "/etc/config/network"])
+        system("cp /etc/config/stp /etc/config/network")
 
-    call(["/etc/init.d/network", "restart"])
+    system("/etc/init.d/network restart")
     stp = not stp
